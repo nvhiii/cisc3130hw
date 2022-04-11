0171565c26c8496a5c9051d85b243623c1b0907d
nvhiii <nahi.khan83@bcmail.cuny.edu>
2022-04-11 14:51:02 -0400

    initiating my homework 2

---
+++ src/hw2.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class hw2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> UserEntry = new ArrayList<>();
+
+        File file = new File("");
+        Scanner stdin = new Scanner();
+
+        while (stdin.hasNext()) {
+
+
+
+
+
+        }
+
+    }
+}
